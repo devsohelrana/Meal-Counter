dfaa9e96e9beff36bdee4fa4c560ebec8a3b31d3
devsohelrana <dev.sohelrana@gmail.com>
2024-08-09 15:23:32 +0600

    Soft Finding Meal Counter Project Test

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      hind: ['Hind Siliguri', 'sans-serif'],
+    },
+    colors: {
+      primary: '#000000',
+      secondary: '#D9D9D9',
+      focus: '#15DFEC'
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
